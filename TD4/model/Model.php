be34e335c8230bd6f7d8686f12039949806e302c
HYNKO Alexis <105347267+TheyAreLostPeople@users.noreply.github.com>
2022-10-16 14:31:38 +0200

    Add files via upload

---
+++ TD4/model/Model.php
@@ -0,0 +1,42 @@
+<?php
+	require_once '../config/Conf.php';
+
+	class Model {
+		
+		public static $pdo;
+		
+		/*
+		public function __constr(){
+			self::Init();
+			
+		}
+		*/
+		
+		public static function Init() {
+			try {
+				self::$pdo = new PDO(
+					'mysql:host='.Conf::getHostname().';dbname='.Conf::getDatabase().';charset=utf8',
+					Conf::getLogin(),
+					Conf::getPassword(),
+					array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+				);
+			} catch(PDOException $e) {
+				echo $e->getMessage(); // affiche un message d'erreur
+				die();
+			}
+			self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			return self::$pdo;
+		}
+		
+		/*
+		public static function getPDO() {
+			if (is_null(self::$pdo)) {
+				self::$pdo = self::Init();
+			}
+			return self::$pdo;
+		}
+		*/
+
+	}
+	
+?>
